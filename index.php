110092fe4f7c5d85440588931cac76580b1da342
lfaust89 <lfaust@110bourgogne.fr>
2020-03-10 12:22:46 +0100

    premier commit lionel

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+<link rel="stylesheet" href="styles/styles.css">
+
+</head>
+<body>
+    <div id="nav-bar">Une navbar</div>
+    <h1>DEMAT breizhouz</h1>
+    Lorem ipsum dolor <span class="green">sit amet</span>, consectetur adipisicing elit. Dicta sapiente ratione veritatis placeat labore esse assumenda rerum tempora eum, facere fuga sequi ipsum natus, provident quibusdam ipsam itaque delectus asperiores?
+</body>
+</html>
