098f4de296e871ddfe4435938b22d1a6559f90d3
lfaust89 <lfaust@110bourgogne.fr>
2020-03-10 12:31:09 +0100

    commit3

--- index.php
+++ index.php
@@ -10,7 +10,7 @@
 <body>
     <div id="nav-bar">Une navbar</div>
     <div id="content">content</div>
-    <div id="footer"><footer></div>
+    <div id="footer"></div>
     <h1>DEMAT breizhouz</h1>
     Lorem ipsum dolor <span class="green">sit amet</span>, consectetur adipisicing elit. Dicta sapiente ratione veritatis placeat labore esse assumenda rerum tempora eum, facere fuga sequi ipsum natus, provident quibusdam ipsam itaque delectus asperiores?
 </body>
